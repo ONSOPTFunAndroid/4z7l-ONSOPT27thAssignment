01f3a9c3c2164d5710218e028aa68943b420ce84
4z7l <7d4z7l@naver.com>
2020-11-09 17:04:20 +0900

    add : bottom navigation & viewpager in Main

--- app/src/main/java/com/igluesmik/sopt/ui/view/MainActivity.kt
+++ app/src/main/java/com/igluesmik/sopt/ui/view/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.igluesmik.sopt.ui.view
 
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.igluesmik.sopt.R
 import com.igluesmik.sopt.databinding.ActivityMainBinding
+import com.igluesmik.sopt.ui.adapter.MainViewPagerAdapter
 import com.igluesmik.sopt.ui.base.BaseActivity
 import com.igluesmik.sopt.ui.base.BaseViewModel
+import com.igluesmik.sopt.ui.view.home.HomeFragment
 import com.igluesmik.sopt.ui.view.profile.ProfileFragment
+import com.igluesmik.sopt.ui.view.settings.SettingsFragment
+import kotlin.properties.Delegates
 
 
 class MainActivity : BaseActivity<ActivityMainBinding, BaseViewModel>() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override val layoutResourceId: Int
         get() = R.layout.activity_main
     override val viewModel = BaseViewModel()
 
+    private lateinit var viewPagerAdapter: MainViewPagerAdapter
+
     override fun initStartView() {
-        startProfileFragment()
-        initNavigation()
+        initBottomNavigation()
+        initViewPager()
     }
 
     override fun initBeforeBinding() {
@@ -35,20 +35,40 @@ class MainActivity : BaseActivity<ActivityMainBinding, BaseViewModel>() {
 
     }
 
-    private fun startProfileFragment() {
-        /*supportFragmentManager.beginTransaction()
-            .add(R.id.fragment, ProfileFragment())
-            .commit()*/
+    private fun initBottomNavigation() {
+        viewDataBinding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            var index by Delegates.notNull<Int>()
+            when (it.itemId) {
+                R.id.menu_home -> index = 0
+                R.id.menu_profile -> index = 1
+                R.id.menu_settings -> index = 2
+            }
+            viewDataBinding.viewPager.currentItem = index
+            true
+        }
     }
 
-    private fun initNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-
-
-        //viewDataBinding.bottomNavigationView.setupWithNavController(navController)
-        NavigationUI.setupWithNavController(viewDataBinding.bottomNavigationView, navController)
-
+    private fun initViewPager() {
+        viewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.setFragmentList(
+            listOf(
+                HomeFragment(),
+                ProfileFragment(),
+                SettingsFragment()
+            )
+        )
+        viewDataBinding.viewPager.adapter = viewPagerAdapter
+        viewDataBinding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+            override fun onPageSelected(position: Int) {
+                viewDataBinding.bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
     }
-
 }
